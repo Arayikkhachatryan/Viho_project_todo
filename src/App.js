1f1f784a8a2bf8613c4e66b30a45aca69757acef
arayikkhachatryan <solnce223@gmail.com>
2022-07-20 15:05:55 +0400

    asd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Header from './components/header/Header';
 import './assets/css/App.css'
+import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
+import TodoField from './components/todoField/TodoField'
 
 function App() {
   return (
     <>
       <Header />
       <Sidebar />
+      <TodoField />
     </>
   );
 }
