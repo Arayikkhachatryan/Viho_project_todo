d5b60c6c9946d3ce2c06155a716d543c7c5a6a2f
arayikkhachatryan <solnce223@gmail.com>
2022-07-20 18:55:09 +0400

    done button func

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,16 +1,20 @@
 import "./FormStyle.css";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { AiOutlineCheck } from "react-icons/ai";
+import { useState } from "react";
 
 import React from "react";
 
 const Form = ({ todos, deleteTodo }) => {
+  const [styleBool, setStyleBool] = useState(false);
   return (
     <>
       {todos.map((todo, index) => (
         <div key={index.toString()} id="some-todo">
           <div className="task-name">
-            <h4>{todo}</h4>
+            <h4 style={
+              styleBool === true ? {textDecoration: "line-through", color: "#26695c"} : {}
+            }>{todo}</h4>
           </div>
           <div className="task-icons">
             <div className="delete">
@@ -22,7 +26,10 @@ const Form = ({ todos, deleteTodo }) => {
               />
             </div>
             <div className="approve">
-              <AiOutlineCheck className="delete-approve" />
+              <AiOutlineCheck className="delete-approve"
+                onClick={() => {
+                  setStyleBool(true);
+                }} />
             </div>
           </div>
         </div>
